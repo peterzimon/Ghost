83fcf04b352d701c7bf73aaf1b1044a93d34dd05
Zimo <peter.zimon@gmail.com>
2019-02-25 16:00:19 +0700

    Updated navigation between subscribe and login page

--- core/server/lib/members/static/auth/pages/PasswordResetSentPage.js
+++ core/server/lib/members/static/auth/pages/PasswordResetSentPage.js
@@ -3,7 +3,7 @@ import FormSubmit from '../components/FormSubmit';
 
 export default ({ error, handleSubmit }) => (
     <div className="gm-modal-form">
-        <FormHeader title="Sign up" error={error} errorText="Unable to send email" />
+        <FormHeader title="Reset password" error={error} errorText="Unable to send email" />
         <Form bindTo="request-password-reset" onSubmit={handleSubmit}>
             <div className="gm-reset-sent">
                 <p>We’ve sent a recovery email to your inbox. Follow the link in the email to reset your password.</p>
